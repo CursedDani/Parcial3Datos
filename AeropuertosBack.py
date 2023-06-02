c32f0674c836084cab132c37a3277706dbba3aa3
CursedDani <dfarangog@gmail.com>
2023-06-01 19:51:23 -0500

    Feature: separate airport by regions

--- AeropuertosBack.py
+++ AeropuertosBack.py
@@ -6,9 +6,35 @@ from Flights import Flight
 import datetime
 import random
 from Dataframe import ExcelDataframe
+import matplotlib.patches as mpatches
 
 class Graph():
 	def __init__(self) -> None:
+		self.regions = {
+    'Andina': ['AXM', 'BOG', 'MDE', 'PEI', 'EOH', 'UIB', 'LET', 'IBE', 'NVA', 'PPN', 'MZL', 'GYM', 'PYA', 'CRC','SJE'],
+    'Caribe': ['BAQ', 'CTG', 'SMR', 'VUP', 'RCH', 'ADZ', 'CZU', 'MTR', 'LMN'],
+    'Pacífico': ['BGA', 'CUC', 'BSC', 'AUC', 'LMC', 'GPI', 'TCO', 'IPI', 'CPB', 'ACD'],
+    'Orinoquia': ['APO', 'VVC', 'EYP', 'MCJ', 'PVA', 'RVE', 'TME', 'PAL', 'BUN', 'PDA', 'MVP','PGT','PCR'],
+    'Amazonia': ['PSO', 'LPD', 'FLA', 'LQM', 'VGZ', 'PUU'],
+    'Insular': ['ADZ', 'PVA'],
+    'Llanos Orientales': ['API', 'VVC', 'EYP', 'MCJ', 'PVA', 'RVE', 'TME', 'PAL', 'BUN', 'PDA', 'MVP', 'NQU', 'CAQ', 'PTX'],
+    'Cafetera': ['AXM', 'MDE', 'PEI', 'UIB', 'MZL']
+}
+
+
+
+		self.colors = {
+    'Andina': 'red',
+    'Caribe': 'blue',
+    'Pacífico': 'green',
+    'Orinoquia': 'orange',
+    'Amazonia': 'purple',
+    'Insular': 'cyan',
+    'Llanos Orientales': 'magenta',
+    'Cafetera': 'brown'
+}
+
+
 		self.df = ExcelDataframe()
 
 		dfc = pd.read_csv('costos.csv', sep=';',header=None,names=['origen','destino','precio'])
@@ -30,27 +56,47 @@ class Graph():
 
 
 	def showGraph(self):
-		dfc = pd.read_csv('costos.csv', sep=';',header=None,names=['origen','destino','precio'])
+		dfc = pd.read_csv('costos.csv', sep=';', header=None, names=['origen', 'destino', 'precio'])
 
-		G = nx.DiGraph() 
-
-		G = nx.from_pandas_edgelist(dfc,"origen","destino","precio",create_using=nx.DiGraph)
+		G = nx.DiGraph()
+		G = nx.from_pandas_edgelist(dfc, "origen", "destino", "precio", create_using=nx.DiGraph)
 
 		self.Graph = G
 
 		self.Graph.edges(data=True)
 		for u, v, attrs in G.edges(data=True):
-			weight_str = attrs['precio'].replace(",","")
+			weight_str = attrs['precio'].replace(",", "")
 			weight_int = int(weight_str)
 			attrs['precio'] = weight_int
+
 		edge_labels = nx.get_edge_attributes(self.Graph, 'precio')
 		pos = nx.spring_layout(self.Graph)
+		unique_nodes = set()
+		for u, v, attrs in G.edges(data=True):
+			unique_nodes.add(u)
+			unique_nodes.add(v)
 
-		nx.draw_networkx_edge_labels(self.Graph,pos,rotate=False,edge_labels=edge_labels)
+		nx.draw_networkx_edges(self.Graph, pos)
 
-		nx.draw(self.Graph,pos,with_labels=True,node_color="lightgreen")
-		
+		regions = list(self.regions.values())
+		region_colors = list(self.colors.values())
+		legend_patches = []
+		for i, region in enumerate(regions):
+			subgraph = G.subgraph(region)
+			nx.draw_networkx_nodes(subgraph, pos, node_color=region_colors[i], nodelist=list(unique_nodes.intersection(subgraph.nodes)))
+			legend_patch = mpatches.Patch(color=region_colors[i], label=list(self.colors.keys())[i])
+			legend_patches.append(legend_patch)
+
+		labels = nx.get_edge_attributes(G, 'precio')
+		nx.draw_networkx_labels(G, pos, font_size=10)
+		nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=8,rotate=False)
+
+		plt.title('Colombian Airports by Region')
+		plt.legend(handles=legend_patches)
+		plt.axis('off')
+
 		plt.show()
+
 
 	def addNewRoute(self,origin,destination,price):
 		line = [origin,destination,price]
@@ -71,19 +117,43 @@ class Graph():
 
 			pos = nx.spring_layout(G)
 
-			nx.draw(G, pos, with_labels=True, node_color='lightgreen')
+			#nx.draw(G, pos, with_labels=True, node_color='lightgreen')
 
 			path_edges = [(shortest_path[i], shortest_path[i + 1]) for i in range(len(shortest_path) - 1)]
 
-			nx.draw_networkx_edge_labels(self.Graph,pos,rotate=False,edge_labels=self.edges)
+			#nx.draw_networkx_edge_labels(self.Graph,pos,rotate=False,edge_labels=self.edges)
+			nx.draw_networkx_edges(self.Graph, pos)
 
-			nx.draw(subGraph,pos,with_labels=True,node_color='lightgreen',edge_color='red',edgelist=path_edges)
+			nx.draw(subGraph,pos,with_labels=False,edge_color='red',edgelist=path_edges)
 			precio = 0
 
 			for u,v,attrs in subGraph.edges(data=True):
 				if (u,v) in path_edges:
 					precio += attrs['precio']
 			print(f'Total cost of flights is: {precio}\n')
+
+			unique_nodes = set()
+			for u, v, attrs in G.edges(data=True):
+				unique_nodes.add(u)
+				unique_nodes.add(v)
+
+
+			regions = list(self.regions.values())
+			region_colors = list(self.colors.values())
+			legend_patches = []
+			for i, region in enumerate(regions):
+				subgraph = G.subgraph(region)
+				nx.draw_networkx_nodes(subgraph, pos, node_color=region_colors[i], nodelist=list(unique_nodes.intersection(subgraph.nodes)))
+				legend_patch = mpatches.Patch(color=region_colors[i], label=list(self.colors.keys())[i])
+				legend_patches.append(legend_patch)
+
+			labels = nx.get_edge_attributes(G, 'precio')
+			nx.draw_networkx_labels(G, pos, font_size=10)
+			nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=8,rotate=False)
+
+			plt.title('Colombian Airports by Region')
+			plt.legend(handles=legend_patches)
+			plt.axis('off')
 
 			plt.show()
 			opt = int(input("Do you want to see tickets to next flight?\n1->Yes\n2->No\n"))
@@ -157,5 +227,7 @@ class Graph():
 					writer.writerow(row)
 
 			return list_of_nearest_fl()
+		
+
 
 
